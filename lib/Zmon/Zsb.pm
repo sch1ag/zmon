80d4c76cc408705965efeb754a4e0c3062eef6cb
Vladimir Kostornoy <kostornoy@gmail.com>
2020-06-17 03:09:02 +0300

    zabbix sender with temp file, better error handling, 1 in run_ok quoted

--- lib/Zmon/Zsb.pm
+++ lib/Zmon/Zsb.pm
@@ -1,15 +1,16 @@
 #!/usr/bin/perl -w
 # Wrapper package for zabbix_sender binary. Implemented as singletone.
-# Version 1
+# Version 3
 package Zmon::Zsb;
 
 use vars;
 use strict;
 use warnings;
 use Sys::Hostname;
+use FindBin qw($Script);
 
 use JSON::PP qw(encode_json);
-use IPC::Run qw( run timeout );
+use IPC::Run qw( run timeout harness );
 #DEBUG use Data::Dumper;
 use Zmon::Slog qw(slog sfatal);
 
@@ -22,6 +23,7 @@ my @configs = ('/etc/opt/zabbix-agent/zabbix_agentd.conf', '/etc/zabbix/zabbix_a
 my $ZBXSENDER_CMD = _get_first_exist_file(\@senders);
 my $ZBX_CFG = _get_first_exist_file(\@configs);
 my $HNAME = _get_zbx_hostname($ZBX_CFG);
+my $sender_input = '/tmp/' . $Script . '.' . $$ . '.zsend';
 
 # Usage: zbx_send('key' => "KEY", 'value' => "VALUE")
 sub zbx_send
@@ -32,19 +34,40 @@ sub zbx_send
         sfatal('msg' => "No key or value");
     }
 
+    my @cmd = ($ZBXSENDER_CMD, '-c', $ZBX_CFG, '-i', $sender_input);
     my @metric = ($HNAME, $params{'key'});
-    my $Toption = '';
     if ($params{'timestamp'})
     {
         push @metric, $params{'timestamp'};
-        $Toption = '-T';
+        push @cmd, '-T';
     };
     push @metric, $params{'value'};
     my $send_string = join(' ', @metric);
 
-    my @cmd = ($ZBXSENDER_CMD, '-c', $ZBX_CFG, $Toption, '-i', '-');
-    my ($out, $err);
-    run \@cmd, \$send_string, \$out, \$err, timeout(30) or sfatal('msg' => "$ZBXSENDER_CMD : $?");    
+    open (my $fh, '>', $sender_input) or sfatal('msg' => "Couldn't open $sender_input : $!");
+    print $fh $send_string."\n" ;
+    close($fh);
+
+    my ($out, $err, $run_ok, $x);
+    my $h = harness \@cmd, \undef, \$out, \$err, timeout(30);
+    eval {
+        $run_ok = run $h;
+        1;
+    };
+    if ($@)
+    {
+        $x = $@;
+        slog(msg => "zabbix_sender exception: $x");
+    }
+    if (! $run_ok)
+    {
+        slog(msg => "zabbix_sender rc: " . $h->result);
+        slog(msg => "zabbix_sender stderr: $err");
+        slog(msg => "zabbix_sender stdout: $out");
+        slog(msg => "zabbix_sender cmd: " . join(' ', @cmd));
+    }
+    close $fh;
+    unlink $sender_input;
 }
 
 sub zbx_jsend
@@ -58,7 +81,7 @@ sub zbx_jsend
 
     if ($params{'addrunok'})
     {
-        push @{$params{'value'}}, {'RUN_OK' => 1};
+        push @{$params{'value'}}, {'RUN_OK' => "1"};
     }
 
     my $data2send;
